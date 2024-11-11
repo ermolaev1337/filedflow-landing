1082a8071b00df9e22e46654b4ed1e2bb561c451
Egor Ermolaev <egorostu@gmail.com>
2024-11-11 02:48:38 +0100

    update(form): anchor to the form from the button on top of the page

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -64,7 +64,7 @@ export default function HeroHome() {
                 >
                   <a
                     className="btn group mb-4 w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="#0"
+                    href="#contactForm"
                   >
                     <span className="relative inline-flex items-center">
                       Book a call{" "}
